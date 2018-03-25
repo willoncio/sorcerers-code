8376a14b741c06b6c332b2c7c13da5f544daae41
codecademy <ccuser@codecademy.com>
2018-03-25 15:21:38 +0000

    Second commit

--- commit_test.rb
+++ commit_test.rb
@@ -5,9 +5,10 @@ begin
   g = Git.open(".")
   g.log(20).count
 rescue
-  puts "No files or directories have been committed."
+  puts "No commit has been made"
   exit 1
 else
   puts "A commit has been made"
   exit 0
 end
+
